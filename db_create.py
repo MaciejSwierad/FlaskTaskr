f224fcea053ff7c162dc351cd22bbe98eec72882
Maciej Swierad <maciej.swierad@gmail.com>
2018-08-09 12:17:39 +0200

    passwordhash

--- db_create.py
+++ db_create.py
@@ -1,14 +1,14 @@
 from project import db
-from project.models import Task, User
-from datetime import date
+# from project.models import Task, User
+# from datetime import date
 
 db.create_all()
 
-db.session.add(
-    User("admin", "ad@min.com", "admin", "admin")
-    )
+# db.session.add(
+#     User("admin", "ad@min.com", "admin", "admin")
+#     )
 
-db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, date(2015, 5, 23), 10, 1))
-db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, date(2015,3,23), 10, 1))
+# db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, date(2015, 5, 23), 10, 1))
+# db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, date(2015,3,23), 10, 1))
 
 db.session.commit()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from project import app, db
+from project import app, db, bcrypt
 from project._config import basedir
 from project.models import User, Task
 
@@ -12,10 +12,13 @@ class TestUsers(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +\
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+        self.assertEquals(app.debug, False)
 
     #executed after each test
     def tearDown(self):
@@ -39,7 +42,7 @@ class TestUsers(unittest.TestCase):
         return self.app.get('logout/', follow_redirects=True)
 
     def create_user(self, name, email, password):
-        new_user = User(name=name, email=email, password=password)
+        new_user = User(name=name, email=email, password=bcrypt.generate_password_hash(password))
         db.session.add(new_user)
         db.session.commit()
 
@@ -54,7 +57,7 @@ class TestUsers(unittest.TestCase):
 
     #tests
     def test_user_setup(self):
-        new_user = User("michael", "michael@herman.org", "michaelherman")
+        new_user = User("michael", "michael@herman.org", bcrypt.generate_password_hash("michaelherman"))
         db.session.add(new_user)
         db.session.commit()
         test = db.session.query(User).all()
